28662c06cf78b09af440580ef04e27ee6a1ddffe
Matthew Bardoe <mbardoe@gmail.com>
2014-10-22 16:49:38 -0400

    Edited Person class
    Put in some instance variables, and a setter and getter method along
    with javadoc comments to show how to do javadoc comments.

--- Person.java
+++ Person.java
@@ -2,11 +2,38 @@
 * This class is part of a simulation of the spread of a virus 
 * through a population. It uses the SIR model to
 * model the spread of virus through a population.
+* @author Matthew Bardoe, and others
+* @version 0.1
+* 
 */
 
 class Person  {
+	private bool isInfected;
+	private bool isAlive;
+	private bool isRecovered;
 
+	/**
+	* A setter method for the infection status of a person.
+	* @param infected a boolean describing whether a person is infected.
+	*/
+	public void setInfected(bool infected)
+	{
+		this.isInfected=infected;
+	}
 
+	/**
+	* A getter method for the infection status of person.
+	* @return boolean indicating the infection state of the person.
+	*/
+	public bool infected()
+	{
+		return isInfected;
+	}
+
+	/**
+	* This main method is meant to test some of the basic implementations
+	* of this class.
+	*/
 	public static void main(String[] args){
 		Person person = new Person();
 		System.out.println(person.susceptible());
