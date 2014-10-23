6c9dbefbd5b38fcba8b7fe7f37aeea83f4295e95
seliason <seliason15@choate.edu>
2014-10-23 12:09:23 -0400

    Update Virus.java

--- Virus.java
+++ Virus.java
@@ -1,17 +1,19 @@
 
 public class Virus {
-	double mortalityRate;
+	double deathRate;
 	double spreadRate;
-	int lengthOfInfection;
+	int infectiousDays;
 
 /**
 *this is a method to get the mortality rate for the virus
-**/
-public double getMortalityRate(){
-	return mortalityRate;
+* @return double that represents the Mortality
+**/ 
+public double getDeathRate(){
+	return deathRate;
 	}
 /**
 *this is a method to get the spread rate for the virus
+*
 **/
 public double getSpreadRate(){
 	return spreadRate;
@@ -20,8 +22,8 @@ public double getSpreadRate(){
 *this is a method to get the duration of the virus in a person
 *
 **/
-public int getLengthOfInfection(){
-	return lengthOfInfection;
+public int getInfectiousDays(){
+	return infectiousDays;
 }
 
 
@@ -29,8 +31,8 @@ public int getLengthOfInfection(){
 	{
 		Virus ebola = new Virus(.20, .50, 10);
 		System.out.println(ebola.getSpreadRate()==.2);
-		System.out.println(ebola.getMortalityRate()==.5);
-		System.out.println(ebola.getLengthOfInfection()==10);
+		System.out.println(ebola.getDeathRate()==.5);
+		System.out.println(ebola.getInfectiousDays==10);
 		System.out.println(ebola); // should return "Spread Rate: .2 Death Rate: .5 Infectious Days: 10"		
 	}
 }
