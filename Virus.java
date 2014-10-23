b9a2ae102e69757ae1936f82a3f5ef0f0d50d30f
Caleb <totesriteous@live.com>
2014-10-23 12:27:01 -0400

    updated finished string

--- Virus.java
+++ Virus.java
@@ -53,7 +53,7 @@ public int getInfectiousDays(){
 		Virus ebola = new Virus(.20, .50, 10);
 		System.out.println(ebola.getSpreadRate()==.2);
 		System.out.println(ebola.getDeathRate()==.5);
-		System.out.println(ebola.getInfectiousDays==10);
-		System.out.println(ebola); // should return "Spread Rate: .2 Death Rate: .5 Infectious Days: 10"		
+		System.out.println(ebola.getInfectiousDays()==10);
+		System.out.println("Spread Rate- "+ebola.getSpreadRate()+" Death rate- "+ebola.getDeathRate()+" Infectious Days- "+ebola.getInfectiousDays()); // should return "Spread Rate: .2 Death Rate: .5 Infectious Days: 10"		
 	}
 }
