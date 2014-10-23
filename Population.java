76a10b95fc5a816d78a0902be80ec0a9886966bc
Yonghyun <Yonghyun@Yonghyunui-MacBook-Pro.local>
2014-10-23 12:28:26 -0400

    Added new method.

--- Population.java
+++ Population.java
@@ -7,40 +7,50 @@
 */
 
 public class Population  {
-	private int populationSize;
-	private Person[] people;
+ private int populationSize;
+ private Person[] people;
+ 
+ 
+ 
+ /**
+ * This method adds a person to the population
+ */
+ public void addPerson();
 
-	/**
-	* This method adds a person to the population
-	*/
-	public void addPerson();
+ /**
+ * This method creates the acquaintances for all the
+ * people in the population.
+ */
+ private void createAquaintances();
 
-	/**
-	* This method creates the acquaintances for all the
-	* people in the population.
-	*/
-	private void createAquaintances();
-
-	/**
-	* This function returns the number of people that are 
-	* infected in the population.
-	* @return int the number of infected people in the population.
-	*/
-	public int numberInfected();
-
-	/**
-	* A main method to test some of the functionality of the 
-	* Population class.
-	*/
-	public static void main(String[] args)
-	{
-		// Create a population of size 1000 where everyone knows
-		// 5 other people.
-		Population population = new Population(1000,5);
-		Person larry = population.getPerson(300);
-		System.out.println(larry);
-		System.out.println(population.numberInfected);
-		System.out.println(population.numberAlive);
-		System.out.println(population.numberRecovered);
-	}
+ /**
+ * This function returns the number of people that are 
+ * infected in the population.
+ * @return int the number of infected people in the population.
+ */
+ public int numberInfected();
+ {
+   int numInfected = 0;
+   for( int i = 0 ; i< populationSize ; i++)
+   {
+     if ( people[i].infected)
+     {
+       numInfected++:
+     }
+   }
+   return numInfected;
+ }
+ 
+ 
+ public static void main(String[] args)
+ {
+  // Create a population of size 1000 where everyone knows
+  // 5 other people.
+  Population population = new Population(1000,5);
+  Person larry = population.getPerson(300);
+  System.out.println(larry);
+  System.out.println(population.numberInfected);
+  System.out.println(population.numberAlive);
+  System.out.println(population.numberRecovered);
+ }
 }
