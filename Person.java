17f93b9a7b5dd74225022ee6d8dafc91671ae183
syeung16 <syeung16@choate.edu>
2014-10-30 12:23:53 -0400

    deleted setDaysInfected

--- Person.java
+++ Person.java
@@ -49,10 +49,6 @@ public class Person  {
 	{
 		this.isRecovered=recovered;
 	}
-	public void setDaysInfected (int day1, int day2)
-	{
-		daysInfected= day2 - day1;
-	}
 
 	/**
 	* A getter method for the infection status of person.
