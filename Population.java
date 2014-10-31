e4712f30bdb2a435f02d77e22a3155d428f3ae4d
seliason <seliason15@choate.edu>
2014-10-31 11:43:53 -0400

    Update Population.java

--- Population.java
+++ Population.java
@@ -46,8 +46,8 @@ public class Population  {
      currentPerson = people[i];
        for (int j = 0; j<numContacts; j++)
         { 
-         int friendIndex = (int)Math.random()*populationSize;
-         Person friend = people[friendIndex];
+         friendIndex = (int)Math.random()*populationSize;
+         friend = people[friendIndex];
          currentPerson.addContact(friend);
         // randomly generate an integer from 
         //friendIndex = Math.random();
